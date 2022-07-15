9efa463ed34f0d7d3e856574888f70cb9190747b
Natalya <trotsyuknata@yandex.ru>
2022-07-15 22:58:17 +0300

    homework

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,7 +1,14 @@
 package batch
 
+//package main
+
 import (
+	"context"
+	"fmt"
+	"sync"
 	"time"
+
+	"golang.org/x/sync/errgroup"
 )
 
 type user struct {
@@ -14,5 +21,52 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+	//const parallel = 5
+	//var gopherID []user
+	errG, _ := errgroup.WithContext(context.Background())
+	errG.SetLimit(int(pool))
+
+	var mu sync.Mutex
+	//c := make(chan user)
+	for i := 0; i < int(n); i++ {
+		j := i
+		//c := make(chan user)
+		errG.Go(func() error {
+			//c <- getOne(int64(j))
+			user := getOne(int64(j))
+
+			mu.Lock()
+			res = append(res, user)
+			mu.Unlock()
+			//c <- res
+			return nil
+		})
+		//gopherID = <-c
+		//res = append(res, gopherID)
+		//err := errG.Wait()
+		//if err := errG.Wait(); err != nil {
+		//	fmt.Printf("Error group error: %s", err.Error())
+		//}
+		//if err != nil {
+		//	fmt.Println(err)
+		//	return nil
+		//}
+	}
+	//for i := 0; i < int(pool); i++ {
+	//	gopherID := <-c
+	//	res = append(res, gopherID)
+	//}
+	if err := errG.Wait(); err != nil {
+		fmt.Printf("Error group error: %s", err.Error())
+	}
+	//fmt.Println(gopherID)
+	//err := errG.Wait()
+	//fmt.Println(err)
+	//return nil
+	return res
+}
+
+func main() {
+	res := getBatch(10, 10)
+	fmt.Println(res)
 }
